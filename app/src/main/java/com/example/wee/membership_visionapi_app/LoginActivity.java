9851292bf9f228311321379eed4a8fd1b55d722f
dongwon <gahlok24@gmail.com>
2018-02-09 17:46:08 +0900

    구글 로그인 추가

---
+++ app/src/main/java/com/example/wee/membership_visionapi_app/LoginActivity.java
@@ -0,0 +1,269 @@
+package com.example.wee.membership_visionapi_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Random;
+
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int RC_SIGN_IN = 1001;
+    private static final String TAG = "LoginActivity";
+
+    private FirebaseDatabase mFirebaseDatabase;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private GoogleApiClient mGoogleApiClient;
+
+    private SignInButton mBtnGoogleSignIn;
+
+    private Button mBtnSignIn;
+    private EditText mEmailField;
+    private EditText mPasswordField;
+
+    private String userName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mBtnSignIn = (Button) findViewById(R.id.btn_signIn);
+        mBtnSignIn.setOnClickListener(this);
+        mEmailField = (EditText) findViewById(R.id.input_id);
+        mPasswordField = (EditText) findViewById(R.id.input_pw);
+
+        initViews();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        initFirebaseAuth();
+        initValues();
+    }
+
+
+    private void initViews() {
+        mBtnGoogleSignIn = (SignInButton) findViewById(R.id.btn_google_signin);
+        mBtnGoogleSignIn.setOnClickListener(this);
+    }
+
+    public void initFirebaseAuth() {
+        mAuth = FirebaseAuth.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                updateProfile();
+            }
+        };
+    }
+
+    private void initValues() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null) {
+            userName = "Guest" + new Random().nextInt(5000);
+        } else {
+            userName = user.getDisplayName();
+        }
+    }
+
+    private void updateProfile() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null) {
+            // 비 로그인 상태 (메시지를 전송할 수 없다.)
+            Toast.makeText(LoginActivity.this, "로그인 하십시오.",
+                    Toast.LENGTH_SHORT).show();
+
+        } else {
+            // 로그인 상태
+            userName = user.getDisplayName();
+            String email = user.getEmail();
+            StringBuilder profile = new StringBuilder();
+            profile.append(userName).append("\n").append(user.getEmail());
+        }
+    }
+
+    private void googleSignIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        updateProfile();
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        ;
+        mAuth.removeAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                GoogleSignInAccount account = result.getSignInAccount();
+                AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+                mAuth.signInWithCredential(credential)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(intent);
+                            }
+                        });
+            } else {
+                updateProfile();
+            }
+        }
+    }
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithEmail:success");
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "!task.isSuccesful()", task.getException());
+
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = mEmailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEmailField.setError("Required.");
+            valid = false;
+        } else {
+            mEmailField.setError(null);
+        }
+
+        String password = mPasswordField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mPasswordField.setError("Required.");
+            valid = false;
+        } else {
+            mPasswordField.setError(null);
+        }
+        return valid;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_google_signin:
+                googleSignIn();
+                Toast.makeText(LoginActivity.this, "Sign In.",
+                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                break;
+
+
+//            case R.id.btn_signUp:
+//                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+//                startActivity(intent);
+//                break;
+
+            case R.id.btn_signIn:
+                Toast.makeText(LoginActivity.this, "SignIn Clicked",
+                        Toast.LENGTH_SHORT).show();
+                signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
+                break;
+        }
+    }
+
+
+}
+
