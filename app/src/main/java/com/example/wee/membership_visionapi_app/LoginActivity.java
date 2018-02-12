e2252b2844914e3c94b2ae460b74ff76e28f27dc
dongwon <gahlok24@gmail.com>
2018-02-12 17:19:03 +0900

    페이스북 로그인 api 추가

--- app/src/main/java/com/example/wee/membership_visionapi_app/LoginActivity.java
+++ app/src/main/java/com/example/wee/membership_visionapi_app/LoginActivity.java
@@ -12,6 +12,13 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -25,6 +32,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
@@ -32,22 +40,27 @@ import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Arrays;
 import java.util.Random;
 
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private static final int RC_SIGN_IN = 1001;
     private static final String TAG = "LoginActivity";
+    private static final String EMAIL = "email";
+
 
     private FirebaseDatabase mFirebaseDatabase;
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private GoogleApiClient mGoogleApiClient;
+    private CallbackManager callbackManager;
 
     private SignInButton mBtnGoogleSignIn;
 
     private Button mBtnSignIn;
+    private Button loginButton;
     private EditText mEmailField;
     private EditText mPasswordField;
 
@@ -62,6 +75,55 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         initFirebaseAuth();
         initValues();
+
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginButton loginButton =  findViewById(R.id.btn_faceBookLogin_singin);
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                handleFacebookAccessToken(loginResult.getAccessToken());
+
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(getApplicationContext(), "페이스북 로그인 성공",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                        }
+
+                        // ...
+                    }
+                });
     }
 
 
@@ -181,6 +243,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 updateProfile();
             }
         }
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     private void signIn(String email, String password) {
@@ -233,6 +296,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
         return valid;
     }
+
 
     @Override
     public void onClick(View v) {
